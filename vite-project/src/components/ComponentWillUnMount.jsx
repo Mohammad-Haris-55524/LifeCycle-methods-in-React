294d8fbc7ff5bca525a8fcdc617700a4b22f9a5b
Mohammad-Haris-55524 <mohammadharis10678@gmail.com>
2024-12-09 23:57:00 +0500

    Component will un mount in detail

---
+++ vite-project/src/components/ComponentWillUnMount.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class ComponentWillUnMount extends Component {
+  render() {
+    return (
+      <div>ComponentWillUnMount</div>
+    )
+  }
+}
+
+export default ComponentWillUnMount
